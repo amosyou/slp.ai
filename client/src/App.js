6fa0b8665321e6247cb965782d91ceda1061d095
Hannah Kuo <hannah.kuo@berkeley.edu>
2022-10-15 21:12:47 -0700

    revert?

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const myFavoriteNumber = 7;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My favorite number is {myFavoriteNumber}
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
