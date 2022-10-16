1cdbc6eeea58b0e6f567d2837b8375c0b1367b9d
Hannah Kuo <hannah.kuo@berkeley.edu>
2022-10-15 21:05:41 -0700

    restore App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Button  from './Button';
-import React, { useState, useEffect, useMemo } from 'react';
-import './styles.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const updateCount = () => {
-    setCount(count + 1);
-  };
-
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  })
-
+  const myFavoriteNumber = 7;
   return (
     <div className="App">
-      <title>DiagnoSelf</title>
       <header className="App-header">
-        <h1>Let's Count!</h1>
-        <button onClick={updateCount}>click me!</button>
-        <p>Count: {count}</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My favorite number is {myFavoriteNumber}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
